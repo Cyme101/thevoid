f62abafbc9b216fb4c768310307488999ca4a5c5
Isabelle Vallerand <isabelle.vall@gmail.com>
2022-09-06 16:32:22 -0400

    Created the Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,76 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
+const Container = styled.div`
+  align-items: center;
+  background-image: url("https://bit.ly/3D3Yc3c");
+  background-position: center no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  opacity: 0.8;
+  width: 100vw;
+`;
 
-const Container = styled.div``
+const Wrapper = styled.div`
+  background-color: white;
+  opacity: 0.9;
+  padding: 30px 20px;
+  width: 25%;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  font-size: 16px;
+  margin: 10px 0;
+  min-width: 40%;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  background-color: #044b7f;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin: 30px 0 10px 0;
+  padding: 15px 20px;
+  width: 30%;
+`;
+
+const Link = styled.a`
+  cursor: pointer;
+  font-size: 16px;
+  margin: 5px 0;
+  text-decoration: none;
+`;
 
 const Login = () => {
   return (
-    <Container>Login</Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Title>LOGIN</Title>
+        <Form>
+          <Input placeholder="Username" />
+          <Input placeholder="Password" />
+          <Link>Forgot your password?</Link>
+          <Button>SIGN IN</Button>
+          <Link>Create account</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,69 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const Container = styled.div``
+const Container = styled.div`
+  align-items: center;
+  background-image: url("https://bit.ly/3D3Yc3c");
+  background-position: center no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  opacity: 0.8;
+  width: 100vw;
+`;
+
+const Wrapper = styled.div`
+  background-color: white;
+  opacity: 0.9;
+  padding: 20px;
+  width: 25%;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  margin: 20px 10px 0 0;
+  min-width: 40%;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  background-color: #044b7f;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+  padding: 15px 20px;
+  width: 30%;
+`;
 
 const Register = () => {
   return (
-    <Container>Register</Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Title>CREATE ACCOUNT</Title>
+        <Form>
+          <Input placeholder="First Name" />
+          <Input placeholder="Last Name" />
+          <Input placeholder="Username" />
+          <Input placeholder="Email" />
+          <Input placeholder="Password" />
+          <Input placeholder="Confirm Password" />
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Register
+export default Register;
