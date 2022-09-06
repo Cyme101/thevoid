5b3eb18b2448dfe4a810ff5757d6c223e99729d1
Isabelle Vallerand <isabelle.vall@gmail.com>
2022-09-06 11:35:20 -0400

    Finalized the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,61 +1,137 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
-const Left = styled.div``;
+import PlaceIcon from "@mui/icons-material/Place";
+import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
+import EmailIcon from "@mui/icons-material/Email";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 20px;
+`;
 
 const Logo = styled.h1``;
 
-const Desc = styled.p``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
 
-const SocialContainer = styled.div``;
+const SocialContainer = styled.div`
+  display: flex;
+`;
 
-const SocialIcon = styled.div``;
+const SocialIcon = styled.div`
+  align-items: center;
+  background-color: #${(props) => props.color};
+  border-radius: 50%;
+  color: white;
+  display: flex;
+  height: 40px;
+  justify-content: center;
+  margin-right: 20px;
+  width: 40px;
+`;
 
-const Center = styled.div``;
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
 
-const Title = styled.h3``;
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
 
-const List = styled.ul``;
+const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
 
-const ListItem = styled.div``;
+const ListItem = styled.li`
+  margin-bottom: 10px;
+  width: 50%;
+`;
 
-const Right = styled.div``;
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
 
-const ContactItem = styled.div``;
-
-const Payment = styled.img``;
+const ContactItem = styled.div`
+  align-items: center;
+  display: flex;
+  margin-bottom: 20px;
+`;
 
 const Footer = () => {
   return (
     <Container>
       <Left>
-        <Logo />
-        <Desc></Desc>
+        <Logo>tHE/vOID</Logo>
+        <Desc>
+          We are going against the grain and we are focused on sustainability.
+          We are using second hand clothes to make edgy and trendy new garments.
+          Each piece will reveal a better version of yourself.
+          <br></br>
+          <br></br>
+          FILL THE VOID WITH STYLE
+        </Desc>
         <SocialContainer>
-          <SocialIcon></SocialIcon>
-          <SocialIcon></SocialIcon>
-          <SocialIcon></SocialIcon>
-          <SocialIcon></SocialIcon>
+          <SocialIcon color="0A80EC">
+            <FacebookRoundedIcon />
+          </SocialIcon>
+          <SocialIcon color="C10174">
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color="1C9CEA">
+            <TwitterIcon />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <PinterestIcon />
+          </SocialIcon>
         </SocialContainer>
       </Left>
       <Center>
-        <Title></Title>
+        <Title>HELPFUL LINKS</Title>
         <List>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
-          <ListItem></ListItem>
+          <ListItem>HOME</ListItem>
+          <ListItem>MY ACCOUNT</ListItem>
+          <ListItem>MY ORDERS</ListItem>
+          <ListItem>MY WISHLIST</ListItem>
+          <ListItem>BAG</ListItem>
+          <ListItem>WOMEN'S</ListItem>
+          <ListItem>MEN'S</ListItem>
+          <ListItem>ACCESSORIES</ListItem>
+          <ListItem>POLICIES</ListItem>
+          <ListItem>TERMS OF SERVICE</ListItem>
+          <ListItem>CHAT WITH OUR TEAM</ListItem>
         </List>
       </Center>
       <Right>
-        <Title></Title>
-        <ContactItem></ContactItem>
-        <ContactItem></ContactItem>
-        <ContactItem></ContactItem>
+        <Title>CONTACT US</Title>
+        <ContactItem>
+          <PlaceIcon style={{ marginRight: "10px" }} /> 61 East Harvey Drive,
+          Fairport, NY 14450
+        </ContactItem>
+        <ContactItem>
+          <PhoneInTalkIcon style={{ marginRight: "10px" }} /> +1 (212) 555-3336
+        </ContactItem>
+        <ContactItem>
+          <EmailIcon style={{ marginRight: "10px" }} /> contact@thevoid
+        </ContactItem>
       </Right>
-      <Payment />
     </Container>
   );
 };
