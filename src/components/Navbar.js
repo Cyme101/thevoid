e7bf684ff2a4aea9c574c6aedf901b72b4e8a518
Isabelle Vallerand <isabelle.vall@gmail.com>
2022-09-09 13:07:43 -0400

    Upgraded node-fetch to 2.6.7

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import Badge from "@mui/material/Badge";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 import { Search } from "@mui/icons-material";
 import { mobile } from "../responsive";
+import { tablet } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
@@ -18,12 +19,14 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   margin-top: 10px;
   ${mobile({ padding: "5px 0px", margin: "8px" })}
+
 `;
 
 const Language = styled.span`
   font: 14px;
   cursor: pointer;
   ${mobile({ display: "none" })}
+  ${tablet({ display: "none" })}
 `;
 
 const Left = styled.div`
